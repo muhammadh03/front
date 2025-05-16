3c969734412b966897761a09aa9557bee7410259
muhammadh03 <muhammadhjalaldeen03@gmail.com>
2025-05-16 13:53:48 +0400

    created flash deals component

---
+++ src/Components/Products/FlashDeals.jsx
@@ -0,0 +1,114 @@
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+// import gamingConsole from "../../assets/products/gaming-console.png";
+
+const FlashDeals = ({ brand, title, price, image, available, sold, rating }) => {
+    return (
+        <div className="w-[10.5rem] md:w-56 lg:w-[17.8rem] h-auto border-2 border-gray-200 rounded-md">
+            <div className="flex flex-col items-center pt-4 px-2">
+                {/* Image */}
+                <div>
+                    <img src={image} alt="" className="h-[5rem] md:h-[10rem]" />
+                </div>
+
+                {/* Countdown */}
+                <div className="w-full flex items-center gap-2 bg-yellow-400/95 text-white text-xs md:text-base rounded-full py-1 px-4 md:px-5 lg:px-8 mt-4">
+                    <span className="flex flex-col">
+                        <span className="font-medium">00</span>
+                        <span className="text-[0.5rem] md:text-xs -mt-[0.1rem] md:-mt-1">Day</span>
+                    </span>
+                    <span className="-mt-4 font-semibold">:</span>
+                    <span className="flex flex-col items-center">
+                        <span className="font-medium">00</span>
+                        <span className="text-[0.5rem] md:text-xs -mt-[0.1rem] md:-mt-1">Hour</span>
+                    </span>
+                    <span className="-mt-4 font-semibold">:</span>
+                    <span className="flex flex-col items-center">
+                        <span className="font-medium">00</span>
+                        <span className="text-[0.5rem] md:text-xs -mt-[0.1rem] md:-mt-1">Min</span>
+                    </span>
+                    <span className="-mt-4 font-semibold">:</span>
+                    <span className="flex flex-col items-center">
+                        <span className="font-medium">00</span>
+                        <span className="text-[0.5rem] md:text-xs -mt-[0.1rem] md:-mt-1">Sec</span>
+                    </span>
+                </div>
+            </div>
+
+            <div className="flex flex-col pt-2 pb-4 px-4">
+                {/* Product Brand */}
+                <div>
+                    <h1 className="text-xs text-gray-500">
+                        {brand}
+                    </h1>
+                </div>
+                {/* Product Title */}
+                <div className="h-13 md:h-24">
+                    <h1 className="text-xs md:text-base font-semibold text-gray-500">
+                        {title}
+                    </h1>
+                </div>
+
+                {/* Product Rating */}
+                <div className="flex items-center mt-1">
+                    <span className="flex gap-1 md:gap-2">
+                        <FaStar className="text-yellow-400" />
+                        <FaStar className="text-yellow-400" />
+                        <FaStar className="text-yellow-400" />
+                        <FaStar className="text-yellow-400" />
+                        <FaStarHalfAlt className="text-yellow-400" />
+                        <span className="text-sm text-gray-500">
+                            {rating}
+                        </span>
+                    </span>
+                </div>
+
+                {/* Product Price */}
+                <div className="flex items-center gap-2 mt-1">
+                    <span className="text-base md:text-lg font-semibold text-gray-500">
+                        ${price}
+                    </span>
+                    <span className="text-sm text-gray-500 line-through">
+                        $1550.00
+                    </span>
+                </div>
+
+                {/* Availability Progress Bar */}
+                <div className="w-full bg-gray-200 rounded-full h-2 mt-2">
+                    {/* Filled Progress */}
+                    <div className="bg-yellow-400 h-2 rounded-full w-[70%]"></div>
+
+                    {/* Dot */}
+                    {/* <div className="absolute top-1/2 -translate-y-1/2 bg-green-700 w-6 h-6 rounded-full border-2 border-white left-[calc(75%-0.75rem)]"></div> */}
+                </div>
+
+                {/* Product Quantity */}
+                <div className="flex items-center justify-between gap-2 mt-1">
+                    <span className="text-xs font-semibold text-gray-400">
+                        Available:
+                        <span className="text-base font-semibold text-gray-600">
+                            {available}
+                        </span>
+                    </span>
+                    <span className="text-xs font-semibold text-gray-400">
+                        Sold:
+                        <span className="text-base font-semibold text-gray-600">
+                            {sold}
+                        </span>
+                    </span>
+                </div>
+
+                {/* Shipping Method */}
+                <div className="flex items-center gap-2 mt-1">
+                    <span className="text-xs font-semibold text-gray-400">
+                        Shipping:
+                    </span>
+                    <span className="text-xs font-semibold text-gray-600">
+                        Free
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FlashDeals;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,9 @@
+import { Link } from 'react-router-dom';
+
 import Hero from '../Components/Layout/Hero';
 import Brands from '../Components/Products/Brands';
 import CategoryCard from '../Components/Products/CategoryCard';
+import FlashDeals from '../Components/Products/FlashDeals';
 
 import mobile from "../assets/products/mobile.png";
 import laptop from "../assets/products/laptop.png";
@@ -14,6 +17,8 @@ import scanner from "../assets/products/scanner.png";
 import faxMachine from "../assets/products/fax-machine.png";
 import gamingFurniture from "../assets/products/gaming-furniture.png";
 import officeFurniture from "../assets/products/office-furniture.png";
+import drone from "../assets/products/drone.png";
+import playstationFive from "../assets/products/spiderman-playstation-5.png";
 
 
 const categoryDetails = [
@@ -77,7 +82,51 @@ const categoryDetails = [
         image: officeFurniture,
         // size: 
     },
-]
+];
+
+
+const flashDealsDetails = [
+    {
+        brand: "Sony",
+        title: "PlayStation 5 Marvel's Spider-Man 2 Limited Edition",
+        price: "560.00",
+        image: playstationFive,
+        available: "38",
+        sold: "12",
+        rating: "4.5",
+        reviews: "12"
+    },
+    {
+        brand: "Lenovo",
+        title: "Lenovo LOQ 15IAX9 Gaming Laptop",
+        price: "650.00",
+        image: laptop,
+        available: "15",
+        sold: "5",
+        rating: "4.8",
+        reviews: "3"
+    },
+    {
+        brand: "DJI",
+        title: "DJI Avatar Fly Smart Combo w/ FPV Goggles V2",
+        price: "970.00",
+        image: drone,
+        available: "8",
+        sold: "4",
+        rating: "4.8",
+        reviews: "4"
+    },
+    {
+        brand: "Dominion",
+        title: "Gaming PC - Intel Core i7-14700KF 20 Cores 28 Threads",
+        price: "3300.00",
+        image: pc,
+        available: "22",
+        sold: "12",
+        rating: "4.9",
+        reviews: "10"
+    }
+];
 
 const Home = () => {
     return (
@@ -99,6 +148,42 @@ const Home = () => {
                             key={title}
                             title={title}
                             image={image}
+                        />
+                    ))}
+                </div>
+            </div>
+
+            <div className='container mx-auto mt-6'>
+                <div className='flex items-center justify-between'>
+                    <div>
+                        <h1 className='text-base md:text-2xl font-bold text-blue-600'>
+                            Flash Deals
+                        </h1>
+                        <p className='w-28 md:w-auto text-xs md:text-base text-gray-500 '>
+                            Special products in this month
+                        </p>
+                    </div>
+
+                    <div className='w-52 md:w-auto flex gap-2 md:gap-4 mt-10.5 md:mt-8 text-[10px] md:text-base text-gray-500'>
+                        <Link to="#" className='hover:text-blue-600'>All</Link>
+                        <Link to="#" className='hover:text-blue-600 truncate'>BEST SELLERS</Link>
+                        <Link to="#" className='hover:text-blue-600 truncate'>MOST VIEWED</Link>
+                        <Link to="#" className='hover:text-blue-600 truncate'>TOP BRANDS</Link>
+                    </div>
+                </div>
+
+                <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-6 mt-6'>
+                    {flashDealsDetails.map(({ brand, title, price, image, available, sold, rating, reviews }) => (
+                        <FlashDeals
+                            key={title}
+                            brand={brand}
+                            title={title}
+                            price={price}
+                            image={image}
+                            available={available}
+                            sold={sold}
+                            rating={rating}
+                            reviews={reviews}
                         />
                     ))}
                 </div>
