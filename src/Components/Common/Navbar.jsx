750050ceec63267b17966120d639e307b27dcaea
muhammadh03 <muhammadhjalaldeen03@gmail.com>
2025-05-18 17:00:24 +0400

    navbar updated

--- src/Components/Common/Navbar.jsx
+++ src/Components/Common/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     return (
         <nav className='max-w-screen mx-auto px-4 sm:px-6 md:px-8 bg-black/85'>
             <div className='text-white text-xs md:text-sm flex items-center justify-between'>
-                <div className='relative bg-yellow-400/95 text-black hover:text-white transition-all duration-300 md:py-3 md:px-4 py-1 px-2  flex items-center gap-2 cursor-pointer'>
+                <div className='relative bg-yellow-300/95 text-black hover:text-white transition-all duration-300 md:py-3 md:px-4 py-1 px-2  flex items-center gap-2 cursor-pointer'>
                     <HiBars3BottomLeft className='h-6 w-6' />
                     <p onClick={toggleMenu} className='md:text-base'>All Categories</p>
                 </div>
@@ -38,7 +38,7 @@ const Navbar = () => {
                     <Link to="#" className='nav-link'>Video Games & Consoles</Link>
                 </div>
 
-                <div className='bg-yellow-400/95 text-black hover:text-white transition-all duration-300 md:py-3 md:px-4 py-1 px-2  flex items-center gap-2 cursor-pointer'>
+                <div className='bg-yellow-300/95 text-black hover:text-white transition-all duration-300 md:py-3 md:px-4 py-1 px-2  flex items-center gap-2 cursor-pointer'>
                     <BiSolidOffer className='h-6 w-6' />
                     <Link to="#" className='md:text-base'>Offers</Link>
                 </div>
