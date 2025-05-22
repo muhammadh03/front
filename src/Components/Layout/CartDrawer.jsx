4674c0cf7306b45a5b99ba9729762484ed22addd
muhammadh03 <muhammadhjalaldeen03@gmail.com>
2025-05-22 17:00:47 +0400

    cart drawer button updated

--- src/Components/Layout/CartDrawer.jsx
+++ src/Components/Layout/CartDrawer.jsx
@@ -26,7 +26,7 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
                 <div className='bg-white sticky bottom-0'>
                     <button
                         // onClick={handleCheckout}
-                        className='w-full bg-blue-500 hover:bg-blue-300 text-white md:p-2 lg:py-3 rounded-lg font-semibold transition'
+                        className='w-full bg-blue-500 hover:bg-blue-300 text-white py-3 rounded-lg font-semibold transition'
                     >
                         Checkout
                     </button>
@@ -36,38 +36,6 @@ const CartDrawer = ({ drawerOpen, toggleCartDrawer }) => {
                 </div>
             </div>
         </div>
-
-
-        // <div className={`fixed top-0 right-0 w-3/4 sm:w-1/2 md:w-[30rem] h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${drawerOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-
-        //     {/* Close Button */}
-        //     <div className='flex justify-end p-4'>
-        //         <button onClick={toggleCartDrawer}>
-        //             <IoMdCloseCircle className='h-6 w-6 text-gray-600' />
-        //         </button>
-        //     </div>
-
-        //     {/* Cart content with scrollable area */}
-        //     <div className='flex-grow p-4 overflow-y-auto'>
-        //         <h2 className='text-2xl font-semibold mb-4'>Your cart</h2>
-
-        //         {/* Components for cart content */}
-        //         {/* <CartContents /> */}
-        //     </div>
-
-        //     {/* Checkout button fixed at the cart bottom */}
-        //     <div className='p-4 bg-white sticky bottom-0'>
-        //         <button
-        //             // onClick={handleCheckout}
-        //             className='w-full bg-black text-white py-3 rounded-lg font-semibold hover:bg-gray-800 transition'
-        //         >
-        //             Checkout
-        //         </button>
-        //         <p className='text-sm tracking-tighter text-gray-500 mt-2'>
-        //             Shipping, taxes, and discount codes calculated at checkout
-        //         </p>
-        //     </div>
-        // </div>
     )
 }
 
