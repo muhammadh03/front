a75e5a5cf88953a4550f7d1e9dce83569ecbb33e
muhammadh03 <muhammadhjalaldeen03@gmail.com>
2025-06-11 08:45:14 +0400

    small update

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -7,7 +7,6 @@ import { FaRegHeart } from 'react-icons/fa';
 import { GoCodeReview } from 'react-icons/go';
 import { LuBriefcaseBusiness } from 'react-icons/lu';
 import { GiMoneyStack } from 'react-icons/gi';
-import MyOrders from './MyOrders';
 import { Link, Outlet } from 'react-router-dom';
 
 const Account = () => {
@@ -19,49 +18,50 @@ const Account = () => {
                 </div>
             </div>
 
-            <div className='container mx-auto mt-8 flex justify-between gap-6'>
-                <div className='w-1/4'>
-                    <div className='bg-blue-100 rounded-md'>
+            <div className='container mx-auto mt-8 flex flex-col lg:flex-row justify-between gap-6'>
+                <div className='w-full lg:w-1/4'>
+                    <div className='bg-blue-100 rounded-md flex flex-col items-center'>
                         <div className='flex flex-col items-center gap-4 p-4'>
                             <img src={userProfile} alt="profile_image" className='w-32' />
                             <div>
                                 <h1 className='font-semibold text-2xl'>Abdullah</h1>
                             </div>
                         </div>
-                        <div className='flex flex-col gap-10 py-4 px-8 text-gray-600'>
-                            <Link to="profile" className='flex items-center gap-3 hover:text-blue-500 cursor-pointer'>
+                        {/* <div className='flex flex-row lg:flex-col gap-10 py-0 lg:py-4 px-8 text-gray-600'> */}
+                        <div className='grid grid-cols-3 md:grid-cols-6 lg:grid-cols-1 gap-10 py-0 lg:py-4 px-8 text-gray-600'>
+                            <Link to="profile" className='flex flex-col lg:flex-row items-center gap-3 hover:text-blue-500 cursor-pointer'>
                                 <CgProfile className='w-6 h-6' />
                                 <h1 className='font-semibold text-base tracking-wide'>Profile</h1>
                             </Link>
-                            <Link to="orders" className='flex items-center gap-3 hover:text-blue-500 cursor-pointer'>
+                            <Link to="orders" className='flex flex-col lg:flex-row items-center gap-3 hover:text-blue-500 cursor-pointer'>
                                 <BsBoxSeam className='w-6 h-6' />
                                 <h1 className='font-semibold text-base tracking-wide'>Orders</h1>
                             </Link>
-                            <Link to="wishlist" className='flex items-center gap-3 hover:text-blue-500 cursor-pointer'>
+                            <Link to="wishlist" className='flex flex-col lg:flex-row items-center gap-3 hover:text-blue-500 cursor-pointer'>
                                 <FaRegHeart className='w-6 h-6' />
                                 <h1 className='font-semibold text-base tracking-wide'>Wishlist</h1>
                             </Link>
-                            <Link to="reviews" className='flex items-center gap-3 hover:text-blue-500 cursor-pointer'>
+                            <Link to="reviews" className='flex flex-col lg:flex-row items-center gap-3 hover:text-blue-500 cursor-pointer'>
                                 <GoCodeReview className='w-6 h-6' />
                                 <h1 className='font-semibold text-base tracking-wide'>Reviews</h1>
                             </Link>
-                            <Link to="business" className='flex items-center gap-3 hover:text-blue-500 cursor-pointer'>
+                            <Link to="business" className='flex flex-col lg:flex-row items-center gap-3 hover:text-blue-500 cursor-pointer'>
                                 <LuBriefcaseBusiness className='w-6 h-6' />
                                 <h1 className='font-semibold text-base tracking-wide'>Business</h1>
                             </Link>
-                            <Link to="payment" className='flex items-center gap-3 hover:text-blue-500 cursor-pointer'>
+                            <Link to="payment" className='flex flex-col lg:flex-row items-center gap-3 hover:text-blue-500 cursor-pointer'>
                                 <GiMoneyStack className='w-6 h-6' />
                                 <h1 className='font-semibold text-base tracking-wide'>Payment</h1>
                             </Link>
                         </div>
-                        <div className='py-4 px-8'>
-                            <button className='w-full bg-blue-500 hover:bg-blue-700 py-2 px-4 cursor-pointer rounded-sm'>
+                        <div className='py-4'>
+                            <button className='w-full bg-blue-500 hover:bg-blue-700 py-2 px-14 cursor-pointer rounded-sm'>
                                 <h1 className='font-semibold text-white text-lg tracking-wide'>Logout</h1>
                             </button>
                         </div>
                     </div>
                 </div>
-                <div className='w-3/4'>
+                <div className='w-full lg:w-3/4'>
                     <Outlet />
                 </div>
             </div>
--- src/Pages/MyOrders.jsx
+++ src/Pages/MyOrders.jsx
@@ -66,17 +66,17 @@ const MyOrders = () => {
             <div className='pb-4'>
                 <h1 className='text-2xl font-semibold text-gray-700'>My Orders</h1>
             </div>
-            <div>
-                <table className='table-auto w-full'>
+            <div className='overflow-x-auto'>
+                <table className='table-auto min-w-full'>
                     <thead>
                         <tr className='bg-blue-500 text-white'>
-                            <th className="py2 px-4 sm:py-3 text-sm text-start">Image</th>
-                            <th className="py2 px-4 sm:py-3 text-sm text-start">Order ID</th>
-                            <th className="py2 px-4 sm:py-3 text-sm text-start">Created</th>
-                            <th className="py2 px-4 sm:py-3 text-sm text-start">Shipping Address</th>
-                            <th className="py2 px-4 sm:py-3 text-sm text-start">Quantity</th>
-                            <th className="py2 px-4 sm:py-3 text-sm text-start">Price</th>
-                            <th className="py2 px-4 sm:py-3 text-sm text-start">Status</th>
+                            <th className="py-1 md:py-2 px-4 text-xs md:text-sm text-start">Image</th>
+                            <th className="py-1 md:py-2 px-4 text-xs md:text-sm text-start">Order ID</th>
+                            <th className="py-1 md:py-2 px-4 text-xs md:text-sm text-start">Created</th>
+                            <th className="py-1 md:py-2 px-4 text-xs md:text-sm text-start">Shipping Address</th>
+                            <th className="py-1 md:py-2 px-4 text-xs md:text-sm text-start">Quantity</th>
+                            <th className="py-1 md:py-2 px-4 text-xs md:text-sm text-start">Price</th>
+                            <th className="py-1 md:py-2 px-4 text-xs md:text-sm text-start">Status</th>
                         </tr>
                     </thead>
                     <tbody className='text-gray-500'>
@@ -87,32 +87,32 @@ const MyOrders = () => {
                                     onClick={() => handleRowClick(order._id)}
                                     className='border-b border-blue-400 hover:bg-blue-50 cursor-pointer'
                                 >
-                                    <td className='py-2 px-4 sm:py-3 text-sm flex items-center justify-center'>
-                                        <img src={order.orderItems[0].image} alt={order.orderItems[0].image} className='w-16 h-14 rounded-md' />
+                                    <td className='py-1 md:py-2 px-4 flex items-center justify-center'>
+                                        <img src={order.orderItems[0].image} alt={order.orderItems[0].image} className='w-20 h-auto rounded-md' />
                                     </td>
-                                    <td className='py-2 px-4 sm:py-3 text-sm'>
+                                    <td className='py-1 md:py-2 px-4 text-xs md:text-sm'>
                                         <p> {order._id} </p>
                                     </td>
-                                    <td className='py-2 px-4 sm:py-3 text-sm'>
+                                    <td className='py-1 md:py-2 px-4 text-xs md:text-sm'>
                                         <p>
                                             {new Date(order.createdAt).toLocaleDateString()}
                                             {" "}
                                             {new Date(order.createdAt).toLocaleTimeString()}
                                         </p>
                                     </td>
-                                    <td className='py-2 px-4 sm:py-3 text-sm'>
+                                    <td className='py-1 md:py-2 px-4 text-xs md:text-sm'>
                                         <p>
                                             {order.shippingAddress ? `${order.shippingAddress.city}, 
                                         ${order.shippingAddress.country}` : "N/A"}
                                         </p>
                                     </td>
-                                    <td className='py-2 px-4 sm:py-3 text-sm text-center'>
+                                    <td className='py-1 md:py-2 px-4 text-xs md:text-sm text-center'>
                                         <p> {order.orderItems[0].quantity} </p>
                                     </td>
-                                    <td className='py-2 px-4 sm:py-3 text-sm'>
+                                    <td className='py-1 md:py-2 px-4 text-xs md:text-sm'>
                                         <p>$ {order.totalPrice} </p>
                                     </td>
-                                    <td className='py-2 px-4 sm:py-3 text-sm'>
+                                    <td className='py-1 md:py-2 px-4 text-sm'>
                                         <p className={`${order.isPaid ? "bg-green-100 text-white" : "bg-red-500 text-white"} bg-green-500 text-white font-semibold py-1 px-2 rounded-sm text-center`}>
                                             {order.isPaid ? "Paid" : "Pending"}
                                         </p>
